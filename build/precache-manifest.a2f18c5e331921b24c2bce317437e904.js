d5e44c4a094e1a2931f5b6477aa25868fff193c0
ali-karami1369 <60441353+ali-karami1369@users.noreply.github.com>
2021-06-15 20:32:57 +0430

    Add files via upload

---
+++ build/precache-manifest.a2f18c5e331921b24c2bce317437e904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4f563ddc4419d6f934c303c3df6681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf0ac8cb4ef0f5c46b6",
+    "url": "/static/css/main.54a0cdb6.chunk.css"
+  },
+  {
+    "revision": "bbafc43989c230f000d5",
+    "url": "/static/js/2.e45d0ac3.chunk.js"
+  },
+  {
+    "revision": "2cf0ac8cb4ef0f5c46b6",
+    "url": "/static/js/main.e558c533.chunk.js"
+  },
+  {
+    "revision": "8e689cd4d3827e59490f",
+    "url": "/static/js/runtime-main.e9c2ae90.js"
+  }
+]);
